682210c5062c6a5c957f95112af3a239fdf42ae5
vizken <viz.ken@gmail.com>
2025-10-05 12:31:51 +0800

    更新

--- GAS_3D12/types.ts
+++
@@ -1,8 +0,0 @@
-
-export type ShapeType = 'cube' | 'sphere' | 'cone' | 'torus' | 'cylinder';
-
-export interface AIShape {
-  shape: ShapeType;
-  color: string;
-  scale: [number, number, number];
-}
